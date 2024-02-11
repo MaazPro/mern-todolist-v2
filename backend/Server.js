a9a82274442839fe6e6de2561eb35c2ca83d2250
MaazPro <maazmuhammad94@gmail.com>
2024-02-11 12:29:04 +0800

    cors changes

--- backend/Server.js
+++ backend/Server.js
@@ -1,21 +1,29 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const routes = require('./routes/TodoRoute')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require("./routes/TodoRoute");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const app = express()
+const app = express();
 const PORT = process.env.port || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://mern-todolist.vercel.app/","*"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 mongoose
-.connect('mongodb+srv://root:1234@cluster0.bvwk0lr.mongodb.net/ToDoApp?retryWrites=true&w=majority')
-.then(()=>console.log('Connected to MongoDB'))
-.catch((err)=>console.log(err));
+  .connect(
+    "mongodb+srv://root:1234@cluster0.bvwk0lr.mongodb.net/ToDoApp?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
 app.use(routes);
 
-app.listen(PORT, ()=>console.log(`Listening on ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
