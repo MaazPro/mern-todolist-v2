358ff44edb9a7ecb4770809334e971c91dd93b84
MaazPro <maazmuhammad94@gmail.com>
2024-02-11 12:51:03 +0800

    cors url changes

--- backend/Server.js
+++ backend/Server.js
@@ -11,7 +11,7 @@ const PORT = process.env.port || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://mern-todolist-v2-dpze.vercel.app/","*"],
+    origin: ["https://mern-todolist-v2-dpze.vercel.app","*"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
